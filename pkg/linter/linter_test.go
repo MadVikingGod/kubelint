aa390a56af9896706e9eba71a1e91eb21cb54494
Aaron Clawson <aaronc@circleci.com>
2020-03-31 11:10:22 -0500

    Fixed linter tests

--- pkg/linter/linter_test.go
+++ pkg/linter/linter_test.go
@@ -2,83 +2,12 @@ package linter
 
 import (
 	"fmt"
+	"github.com/madvikinggod/kubelint/pkg/rules/builtin"
 	"io"
+	"sigs.k8s.io/kustomize/kyaml/kio"
 	"strings"
 	"testing"
 )
-
-//func Test_linter_lintObjects(t *testing.T) {
-//
-//	type msg struct {
-//		message    string
-//		isCritical bool
-//	}
-//	tests := []struct {
-//		name string
-//		objs []*unstructured.Unstructured
-//		want []msg
-//	}{
-//		{
-//			name: "No Objects should return no messages",
-//			want: []msg{},
-//		},
-//		{
-//			name: "Unknown object should return not found warning",
-//			objs: []*unstructured.Unstructured{
-//				&unstructured.Unstructured{Object: map[string]interface{}{
-//					"kind": "Carp",
-//					"metadata": map[string]interface{}{
-//						"name": "bob",
-//					},
-//				}},
-//			},
-//			want: []msg{
-//				{
-//					message:    "NoRuleFound - Did not find any rules for - /Carp /bob",
-//					isCritical: false,
-//				},
-//			},
-//		},
-//		{
-//			name: "Pod should return NakedPodCheck message",
-//			objs: []*unstructured.Unstructured{
-//				&unstructured.Unstructured{Object: map[string]interface{}{
-//					"apiVersion": "v1",
-//					"kind":       "pod",
-//					"metadata": map[string]interface{}{
-//						"name": "pod-name",
-//					},
-//				}},
-//			},
-//			want: []msg{
-//				{
-//					message:    "NakedPodCheck - Pods should not be used directly. Use a deployment instead - v1/pod /pod-name",
-//					isCritical: true,
-//				},
-//			},
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			l := &linter{
-//				cfg: Config{Rules: builtin.DefaultRules},
-//			}
-//			got := l.lintObjects(tt.objs)
-//			if len(got) != len(tt.want) {
-//				t.Errorf("lintObjects() returned %d items, wanted %d, got = %v, wanted = %v", len(got), len(tt.want), got, tt.want)
-//				return
-//			}
-//			for i := range got {
-//				if got[i].String() != tt.want[i].message {
-//					t.Errorf("Message missmatch: got = %s, want = %s", got[i].String(), tt.want[i].message)
-//				}
-//				if got[i].IsCritical() != tt.want[i].isCritical {
-//					t.Errorf("Message critical: got = %t, want = %t", got[i].IsCritical(), tt.want[i].isCritical)
-//				}
-//			}
-//		})
-//	}
-//}
 
 func Test_readObjects(t *testing.T) {
 	type args struct {
@@ -130,6 +59,77 @@ kind: otherTest`)},
 			}
 			if len(got) != tt.wantLen {
 				t.Errorf("readObjects() len(got) = %d, want %v", len(got), tt.wantLen)
+			}
+		})
+	}
+}
+
+func Test_linter_lintObjects(t *testing.T) {
+
+	type args struct {
+		yaml string
+	}
+	type msg struct {
+		message    string
+		isCritical bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want []msg
+	}{
+		{
+			name: "No Objects should return no messages",
+			want: []msg{},
+		},
+		{
+			name: "Unknown object should return not found warning",
+			args: args{
+				yaml: `kind: Carp
+metadata:
+  name: bob`,
+			},
+			want: []msg{
+				{
+					message:    "Linting - No rule found - {bob   Carp}",
+					isCritical: false,
+				},
+			},
+		},
+		{
+			name: "",
+			args: args{
+				yaml: `apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-name`,
+			},
+			want: []msg{
+				{
+					message:    "NakedPodCheck - Pods should not be used directly, apps/v1 Deployments are recommended - {pod-name  v1 Pod}",
+					isCritical: true,
+				},
+			},
+		},
+	}
+	l := &linter{
+		cfg: Config{Rules: builtin.DefaultKRules},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			objs, _ := (&kio.ByteReader{Reader: strings.NewReader(tt.args.yaml)}).Read()
+			got := l.lintObjects(objs)
+			if len(got) != len(tt.want) {
+				t.Errorf("lintObjects() returned %d items, wanted %d, got = %v, wanted = %v", len(got), len(tt.want), got, tt.want)
+				return
+			}
+			for i := range got {
+				if got[i].String() != tt.want[i].message {
+					t.Errorf("Message missmatch: got = %s, want = %s", got[i].String(), tt.want[i].message)
+				}
+				if got[i].IsCritical() != tt.want[i].isCritical {
+					t.Errorf("Message critical: got = %t, want = %t", got[i].IsCritical(), tt.want[i].isCritical)
+				}
 			}
 		})
 	}
